4c197df43cdda16a1e61fcbadf26ac45d1cdbc1e
Waziwo <166222021+Waziwo@users.noreply.github.com>
2024-11-03 18:09:19 +0100

    Update firebase-config.js

--- src/js/config/firebase-config.js
+++ src/js/config/firebase-config.js
@@ -1,8 +1,3 @@
-// src/js/firebase-config.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
 export const firebaseConfig = {
     apiKey: "AIzaSyDNtXbYetm8aLRmomgBlVP6HXNZyhttFfQ",
     authDomain: "strona-do-zapisywania-wynikow.firebaseapp.com",
@@ -12,7 +7,3 @@ export const firebaseConfig = {
     appId: "1:30761717995:web:ac03840376114c5fbdeeae",
     measurementId: "G-4F2LGNY193"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
