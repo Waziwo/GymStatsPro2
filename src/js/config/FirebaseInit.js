5730c39f70fb8396d17be98e6c2379298d58da51
Waziwo <166222021+Waziwo@users.noreply.github.com>
2024-11-11 17:37:53 +0100

    1

--- src/js/config/FirebaseInit.js
+++ src/js/config/FirebaseInit.js
@@ -1,9 +1,12 @@
+// src/js/config/FirebaseInit.js
+import dotenv from 'dotenv';
+dotenv.config(); // Wczytaj zmienne z pliku .env
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import dotenv from 'dotenv';
-dotenv.config();
 
+// Użyj zmiennych środowiskowych
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -14,6 +17,7 @@ const firebaseConfig = {
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
+// Inicjalizacja Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
