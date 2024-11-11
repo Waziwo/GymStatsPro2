025651287d40dec1aa1b471ef16c2d6863660637
Waziwo <166222021+Waziwo@users.noreply.github.com>
2024-11-11 17:30:05 +0100

    Update FirebaseInit.js

--- src/js/config/FirebaseInit.js
+++ src/js/config/FirebaseInit.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
