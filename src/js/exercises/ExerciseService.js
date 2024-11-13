b3ef982157a4d69885c49e4e7de4c21ceb1e7388
Waziwo <166222021+Waziwo@users.noreply.github.com>
2024-11-13 19:55:11 +0100

    Update ExerciseService.js

--- src/js/exercises/ExerciseService.js
+++ src/js/exercises/ExerciseService.js
@@ -18,22 +18,18 @@ export class ExerciseService {
         }
     }
 
-   async getExercises(userId) {
-    try {
-        console.log('Getting exercises for user:', userId);
-        const exercisesSnapshot = await getDocs(this.exercisesCollection);
-        console.log('Exercises snapshot:', exercisesSnapshot);
-        console.log('Number of exercises:', exercisesSnapshot.docs.length);
-        const exercises = exercisesSnapshot.docs
-            .map(doc => ({ id: doc.id, ...doc.data() }))
-            .filter(exercise => exercise.userId === userId); // Filtruj ćwiczenia dla konkretnego użytkownika
-        console.log('Number of exercises for user:', exercises.length);
-        console.log('Exercises for user:', exercises);
-        return exercises;
-    } catch (error) {
-        console.error('Error getting exercises:', error);
-        console.error('Error details:', error.message);
-        throw error;
+    async getExercises(userId) {
+        try {
+            console.log('Getting exercises for user:', userId);
+            const exercisesSnapshot = await getDocs(this.exercisesCollection);
+            const exercises = exercisesSnapshot.docs
+                .map(doc => ({ id: doc.id, ...doc.data() }))
+                .filter(exercise => exercise.userId === userId); // Filtruj ćwiczenia dla konkretnego użytkownika
+            return exercises;
+        } catch (error) {
+            console.error('Error getting exercises:', error);
+            throw error;
+        }
     }
-}
+    
 }
